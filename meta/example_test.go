d759c6c68a0df11f7559dd68b68a6a9f9daa410a
Flily Hsu <1126143+flily@users.noreply.github.com>
2022-11-18 02:47:19 +0800

    update example that why we need a new IsNil

--- meta/example_test.go
+++ meta/example_test.go
@@ -3,6 +3,7 @@ package meta
 
 import (
 	"fmt"
+	"runtime"
 )
 
 func ExampleIsUntypedNil() {
@@ -33,4 +34,73 @@ func ExampleIsTypedNil() {
 	// false
 	// true
 	// false
+}
+
+func ExampleIsNil_why() {
+	var err error
+	fmt.Printf("(nil == nil) -> %v\n", err == nil) // true
+	fmt.Printf("type = %T\n", err)                 // <nil>
+	// err is an untyped nil now.
+	u1, t1 := IsNil(err)
+	fmt.Printf("IsNil(err) -> %v, %v\n", u1, t1) // true, false
+
+	// type defined outside of block:
+	// type exampleError struct{}
+	// func (e *exampleError) Error() string { return "example error" }
+	err = func() error {
+		// some function returns an error but you don't know what type it actually is.
+		return func() error {
+			// n is a typed nil
+			var n *runtime.TypeAssertionError = nil
+			return n
+		}()
+	}()
+	fmt.Printf("(nil == nil) -> %v\n", err == nil) // false
+	fmt.Printf("type = %T\n", err)                 // *runtime.TypeAssertionError
+	// err is a typed nil now.
+	u2, t2 := IsNil(err)
+	fmt.Printf("IsNil(err) -> %v, %v\n", u2, t2) // false, true
+
+	// Output:
+	// (nil == nil) -> true
+	// type = <nil>
+	// IsNil(err) -> true, false
+	// (nil == nil) -> false
+	// type = *runtime.TypeAssertionError
+	// IsNil(err) -> false, true
+}
+
+func ExampleIsNil_how() {
+	var err error
+
+	if err == nil {
+		fmt.Println("err == nil")
+	}
+
+	if u, t := IsNil(err); u || t {
+		fmt.Println("err is nil")
+	}
+
+	err = func() error {
+		// some function returns an error but you don't know what type it actually is.
+		return func() error {
+			// n is a typed nil
+			var n *runtime.TypeAssertionError = nil
+			return n
+		}()
+	}()
+
+	if err != nil {
+		fmt.Println("err != nil")
+	}
+
+	if u, t := IsNil(err); u || t {
+		fmt.Println("err is nil")
+	}
+
+	// Output:
+	// err == nil
+	// err is nil
+	// err != nil
+	// err is nil
 }
